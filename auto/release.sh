c09c0311c4318b642524849f3c5140981dbe429f
winlin <winlin@vip.126.com>
2019-12-02 16:09:40 +0800

    Update

--- auto/release.sh
+++ auto/release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-git tag -d release-vdev
-git push origin :release-vdev
-git push aliyun :release-vdev
+git tag -d release-vdev6
+git push origin :release-vdev6
+git push aliyun :release-vdev6
 
-git tag release-vdev
-git push origin release-vdev
-git push aliyun release-vdev
+git tag release-vdev6
+git push origin release-vdev6
+git push aliyun release-vdev6
