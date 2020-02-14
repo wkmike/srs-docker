82f8b4e3208ae6c0dc81d68947faccf75413a070
winlin <winlin@vip.126.com>
2020-02-14 19:27:41 +0800

    Update

--- auto/release.sh
+++ auto/release.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-git tag -d release-vencoder
-git tag release-vencoder
-
 # For docker hub.
 if [[ $MACOS == YES ]]; then
   sed -i '' "s|^ARG repo=.*$|ARG repo=ossrs/srs:dev|g" Dockerfile
 else
   sed -i "s|^ARG repo=.*$|ARG repo=ossrs/srs:dev|g" Dockerfile
 fi
+
+git commit -am "Release encoder to docker hub"; git push
+git tag -d release-vencoder
+git tag release-vencoder
 git push origin -f release-vencoder
 
 # For aliyun hub.
@@ -17,4 +18,8 @@ if [[ $MACOS == YES ]]; then
 else
   sed -i "s|^ARG repo=.*$|ARG repo=registry.cn-hangzhou.aliyuncs.com/ossrs/srs:dev|g" Dockerfile
 fi
+
+git commit -am "Release encoder to aliyun hub"; git push
+git tag -d release-vencoder
+git tag release-vencoder
 git push aliyun -f release-vencoder
